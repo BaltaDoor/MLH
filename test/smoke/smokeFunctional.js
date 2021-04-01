7a71e03edc6736cab5634deb9a4ac80404186ab9
Admin <tudor.b28@gmail.com>
2021-04-01 11:30:04 -0400

    added testData, removed hard code from TC-026

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,17 +1,18 @@
 import sel from '../../data/selectors';
-
+import {name, gender, age, story} from '../../data/testData';
 describe('Required fields and story created ', function () {
 
     before('Open App', function () {
         browser.url('');
     });
 
+
     it('TC-026 Submit button is enabled after fields 1-4 are filled in with valid values', function () {
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
+        $(sel.name).setValue(name.default);
+        $$(sel.radioButtons)[gender.she].click();
+        $(sel.age).setValue(age.default);
         $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        $$(sel.storyList)[story.comedy].click();
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(true);
     });
